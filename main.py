b065d4ac0c38d8d60193298830108ecd6eb8e046
Andrew Szeto <andrew@jabagawee.com>
2017-09-26 22:37:09 -0700

    Changed to reading ENV['SMS_WEBHOOK_PATH'] directly instead of as a config file

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
 
+import os
+
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
-sms_webhook_path_secret = app.config.from_envvar('SMS_WEBHOOK_PATH')
-@app.route(f'/sms_receive/{sms_webhook_path_secret}')
+
+@app.route(f'/sms_receive/{os.environ["SMS_WEBHOOK_PATH"]}')
 def receive_sms_webhook():
     return 'This path receives the SMSes'
